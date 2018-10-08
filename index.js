b834118214fac24976080970a0b2b35556e5a530
Macauley Drake <macauleyann@gmail.com>
2018-10-08 18:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function moveDodgerRight(e) {
  
   if (e.which === 39)
   {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(rightNumbers, 10)
-  console.log(rightNumbers)
+  console.log(leftNumbers)
   if (right > 0) {
     console.log("inside right")
     dodger.style.right = "${right - 1}px"
