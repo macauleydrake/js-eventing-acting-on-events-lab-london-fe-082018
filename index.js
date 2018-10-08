900a32d1b1ee9b6ac9a11476a2a72dd7bc959add
Macauley Drake <macauleyann@gmail.com>
2018-10-08 18:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function moveDodgerRight(e) {
   {
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
- 
+  }
   if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
