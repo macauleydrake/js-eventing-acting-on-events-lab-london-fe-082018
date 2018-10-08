2f736fade8bcc800500e50e526468990c4f87923
Macauley Drake <macauleyann@gmail.com>
2018-10-08 18:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var dodger = document.getElementById('dodger')
-function moveDodgerRight(e) {
-  if (e.which === 39) {
+function moveDodgerRight() {
+  
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
-  }
+  
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
 }
 
-document.addEventListener("keydown", (e)=>moveDodgerRight(e)
+document.addEventListener("keydown", (e)=>
+if (e.which === 39) {
+moveDodgerRight()}
 )
