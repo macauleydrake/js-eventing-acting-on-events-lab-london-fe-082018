95438e5bf449919e25b4a94b1cd195f57ccf1801
Macauley Drake <macauleyann@gmail.com>
2018-10-08 18:29:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+var dodger = document.getElementById('dodger')
+function moveDodgerRight() {
+  
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener("keydown", (e)=> {
+if (e.which === 39) {
+moveDodgerRight()}}
+)
