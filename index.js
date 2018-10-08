93bfd94eb324c127d5b8254e98f4302cfc3bb584
Macauley Drake <macauleyann@gmail.com>
2018-10-08 18:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var dodger = document.getElementById('dodger')
-function moveDodgerRight() {
+function moveDodgerRight(e) {
+  if (e.which === 39) {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+  }
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
